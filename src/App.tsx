252652bee17b00d79c8df8f429a8f230975d124a
Florian Richard <florian.richard@aloe-corp.com>
2024-09-18 18:02:30 +0200

    Add Helmet

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,19 @@ import { useNavigate, useHref, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { NewTraining } from './pages/NewTraining'
 import { Stats } from './pages/Stats'
+import { Helmet } from 'react-helmet-async'
 
 function App() {
     const navigate = useNavigate()
 
     return (
         <NextUIProvider navigate={navigate} useHref={useHref}>
-            <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1' />
+            <Helmet>
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1, maximum-scale=1'
+                />
+            </Helmet>
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/new_training' element={<NewTraining />} />
