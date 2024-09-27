d16befb4c19eb2b6277ffecde40834a3d89a28a5
Florian Richard <florian.richard@aloe-corp.com>
2024-09-27 11:44:42 +0200

    Add page transition animation

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,19 @@
 import { ChartBarIcon, HomeIcon, PlusCircleIcon, UserIcon } from '@heroicons/react/24/solid'
 import { Spacer } from '@nextui-org/react'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Outlet, useLocation } from 'react-router-dom'
 import { ProtectedRoute } from '../features/auth/ProtectedRoute/ProtectRoute'
 import { NavBarButton } from './NavBarButton'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
+import './Layout.style.css'
 
 export const Layout = () => {
     const [activeButton, setActiveButton] = useState<string>('home')
+
+    const [inProp, setInProp] = useState(true)
+
+    const nodeRef = useRef(null)
+
     const location = useLocation()
 
     const handleClick = (buttonName: string) => {
@@ -37,12 +44,30 @@ export const Layout = () => {
         console.log(location.pathname)
     }, [location])
 
+    // useEffect(() => {
+    //     setInProp(false) // Commence la transition de sortie
+    //     setTimeout(() => {
+    //         setInProp(true) // Après la sortie, déclenche la transition d'entrée
+    //     }, 100)
+    // }, [location.pathname])
+
     return (
         <div className='relative min-h-full max-h-full h-full w-full sm:w-[50%] flex flex-col justify-between mx-auto'>
-            <div className='relative flex flex-col flex-grow min-w-full max-h-[89%] h-[89%]  px-8'>
-                <Spacer y={4} />
-                <ProtectedRoute>{<Outlet />}</ProtectedRoute>
-            </div>
+            <SwitchTransition>
+                <CSSTransition
+                    ref={nodeRef}
+                    key={location.pathname}
+                    classNames='fade'
+                    timeout={200}>
+                    <div
+                        className='relative flex flex-col flex-grow min-w-full max-h-[89%] h-[89%]  px-8'
+                        ref={nodeRef}>
+                        <Spacer y={4} />
+
+                        <ProtectedRoute>{<Outlet />}</ProtectedRoute>
+                    </div>
+                </CSSTransition>
+            </SwitchTransition>
 
             <div className='flex flex-row w-full justify-evenly py-2 pb-6 sticky border-t-1 bg-background'>
                 <NavBarButton
