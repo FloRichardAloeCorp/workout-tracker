af3bda2d69734089f6c721a2f2c341520e739375
Florian Richard <florian.richard@aloe-corp.com>
2024-10-02 16:56:15 +0200

    removing not necessary useEffect

--- src/components/features/exercise-progression/SetEvolutionTable/SetEvolutionTable.tsx
+++ src/components/features/exercise-progression/SetEvolutionTable/SetEvolutionTable.tsx
@@ -10,76 +10,68 @@ export interface ISetEvolutionTableProps {
 }
 
 export function SetEvolutionTable(props: ISetEvolutionTableProps) {
-    const [rows, setRows] = React.useState<Row[]>([])
-    const [columns, setColumns] = React.useState<Column[]>([])
+    const rows: Row[] = [
+        {
+            id: 'Poids',
+            unit: 'kg',
+            data: {},
+        },
+        {
+            id: 'Reps',
+            unit: 'reps',
+            data: {},
+        },
+    ]
 
-    React.useEffect(() => {
-        const rows: Row[] = [
-            {
-                id: 'Poids',
-                unit: 'kg',
-                data: {},
-            },
-            {
-                id: 'Reps',
-                unit: 'reps',
-                data: {},
-            },
-        ]
+    const columns: Column[] = [
+        {
+            name: 'Type',
+            key: 'type',
+        },
+    ]
 
-        const columns: Column[] = [
-            {
-                name: 'Type',
-                key: 'type',
-            },
-        ]
+    let setsCount = props.newSets.length
+    if (props.baseSets.length > setsCount) {
+        setsCount = props.baseSets.length
+    }
 
-        let setsCount = props.newSets.length
-        if (props.baseSets.length > setsCount) {
-            setsCount = props.baseSets.length
+    for (let i = 0; i < setsCount; i++) {
+        let key = `set_${i}`
+        columns.push({
+            name: `Série ${i + 1}`,
+            key: key,
+        })
+
+        let baseSetWeight = 0
+        if (props.baseSets[i] != null) {
+            baseSetWeight = props.baseSets[i].weight
         }
 
-        for (let i = 0; i < setsCount; i++) {
-            let key = `set_${i}`
-            columns.push({
-                name: `Série ${i + 1}`,
-                key: key,
-            })
-
-            let baseSetWeight = 0
-            if (props.baseSets[i] != null) {
-                baseSetWeight = props.baseSets[i].weight
-            }
-
-            let newSetWeight = 0
-            if (props.newSets[i] != null) {
-                newSetWeight = props.newSets[i].weight
-            }
-
-            rows[0].data[key] = {
-                baseValue: baseSetWeight,
-                newValue: newSetWeight,
-            }
-
-            let baseSetReps = 0
-            if (props.baseSets[i] != null) {
-                baseSetReps = props.baseSets[i].repetitions
-            }
-
-            let newSetReps = 0
-            if (props.newSets[i] != null) {
-                newSetReps = props.newSets[i].repetitions
-            }
-
-            rows[1].data[key] = {
-                baseValue: baseSetReps,
-                newValue: newSetReps,
-            }
+        let newSetWeight = 0
+        if (props.newSets[i] != null) {
+            newSetWeight = props.newSets[i].weight
         }
 
-        setColumns(columns)
-        setRows(rows)
-    }, [props.baseSets, props.newSets])
+        rows[0].data[key] = {
+            baseValue: baseSetWeight,
+            newValue: newSetWeight,
+        }
+
+        let baseSetReps = 0
+        if (props.baseSets[i] != null) {
+            baseSetReps = props.baseSets[i].repetitions
+        }
+
+        let newSetReps = 0
+        if (props.newSets[i] != null) {
+            newSetReps = props.newSets[i].repetitions
+        }
+
+        rows[1].data[key] = {
+            baseValue: baseSetReps,
+            newValue: newSetReps,
+        }
+    }
 
     return (
         <div className='px-1'>
--- src/components/features/stats/WeeklyTrainingsCard/WeeklyTrainingsCard.tsx
+++ src/components/features/stats/WeeklyTrainingsCard/WeeklyTrainingsCard.tsx
@@ -1,8 +1,6 @@
 import { Card, CardHeader, CardBody } from '@nextui-org/react'
-import * as React from 'react'
 import { WeeklyTrainingBarCharts } from './elements/WeeklyTrainingBarChart'
 import { Profile } from '../../../../type'
-import { WeeklyChartDataPoint } from './elements/WeeklyTrainingBarChart.type'
 import { formatTrainingDuration } from './elements/WeeklyTrainingBarChart.utils'
 import { getChartDataFromProfile } from './WeeklyTrainingsCard.utils'
 
@@ -11,11 +9,7 @@ export interface IWeeklyTrainingsCardProps {
 }
 
 export function WeeklyTrainingsCard(props: IWeeklyTrainingsCardProps) {
-    const [chartData, setChartData] = React.useState<WeeklyChartDataPoint[]>([])
-
-    React.useEffect(() => {
-        setChartData(() => getChartDataFromProfile(props.profile))
-    }, [props.profile])
+    const chartData = getChartDataFromProfile(props.profile)
 
     const trainingsDurationSum = formatTrainingDuration(
         chartData.reduce((acc, training) => (acc += training.value), 0)
